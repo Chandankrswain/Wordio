526b7cf1bb6a7f764e28f2737c23b455994e4910
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2025-07-11 01:56:57 +0530

    ADD: the logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import SearchBar from "../components/search-bar";
+import logo from "../assets/logo.png";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col md:w-[40%] mx-auto justify-between h-screen p-2 bg-yellow-500">
-      <h1>Wordio</h1>
-      <SearchBar />
+      <img src={logo} alt="Wordio Logo" className="w-[160px] h-[160px]" />
+      <div>
+        <p className="text-[80px] font-bold leading-20">Search Here</p>
+        <SearchBar />
+      </div>
     </div>
   );
 };
