568a6e37d1ce7f52273b7daa5da22b3dd1511896
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2025-07-11 01:17:51 +0530

    ADD: created a new homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import SearchBar from "./components/search-bar";
-
+import HomePage from "./pages/HomePage";
 function App() {
   return <HomePage />;
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import SearchBar from "../components/search-bar";
+
+const HomePage = () => {
+  return (
+    <div className="home-page ">
+      <h1>Wordio</h1>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default HomePage;
