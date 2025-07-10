61895c7a30d7eff41b84dd6a499700a95aa5e6a9
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2025-07-11 02:19:46 +0530

    resize the logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,10 @@ import logo from "../assets/logo.png";
 const HomePage = () => {
   return (
     <div className="flex flex-col md:w-[40%] mx-auto justify-between h-screen p-2 bg-yellow-500">
-      <img src={logo} alt="Wordio Logo" className="w-[160px] h-[160px]" />
-      <div>
-        <p className="text-[80px] font-bold leading-20">Search Here</p>
+      <img src={logo} alt="Wordio Logo" className="w-44 h-10 ml-2 mt-6" />
+      <div className="flex flex-col">
+        <p className="text-[80px] font-bold leading-20">Search</p>
+        <p className="text-[40px] font-bold leading-20">Word</p>
         <SearchBar />
       </div>
     </div>
