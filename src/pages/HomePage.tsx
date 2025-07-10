f799c292dcff913569320a6fbaae372421b153cc
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2025-07-11 01:27:09 +0530

    centered the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import SearchBar from "../components/search-bar";
 
 const HomePage = () => {
   return (
-    <div className="home-page ">
+    <div className="flex flex-col md:w-[40%] mx-auto justify-between h-screen p-2 bg-yellow-500">
       <h1>Wordio</h1>
       <SearchBar />
     </div>
