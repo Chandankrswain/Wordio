58293faa8c8fa8a402c7d2bee93b847eee98c6ee
chandankrswain <mr.chandanswain@gmail.com>
2025-07-12 01:22:45 +0530

    ADD: custom font in tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,8 +6,8 @@ const HomePage = () => {
     <div className="flex flex-col md:w-[40%] mx-auto justify-between h-screen p-2 bg-yellow-500">
       <img src={logo} alt="Wordio Logo" className="w-44 h-10 ml-2 mt-6" />
       <div className="flex flex-col">
-        <p className="text-[80px] font-bold leading-20">Search</p>
-        <p className="text-[40px] font-bold leading-20">Word</p>
+        <p className="text-[80px] font-bold leading-20 font-display">Search</p>
+        <p className="text-[40px] font-bold leading-20 font-display">Word.</p>
         <SearchBar />
       </div>
     </div>
