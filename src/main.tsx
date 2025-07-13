8b1357aff933a588f612f25ebf0b592c32cd813c
chandankrswain <mr.chandanswain@gmail.com>
2025-07-14 00:00:48 +0530

    set the routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "./pages/home-page.tsx";
+import CardInfoPage from "./pages/card-info-page.tsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomePage,
+  },
+
+  {
+    path: "/:word",
+    Component: CardInfoPage,
+  },
+]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
   </StrictMode>
 );
