d60731d7ea88bb8e449fa7334779578b9c9d34f0
chandankrswain <mr.chandanswain@gmail.com>
2025-07-18 01:19:53 +0530

    fixed minor bugs

--- src/pages/card-info-page.tsx
+++ src/pages/card-info-page.tsx
@@ -54,7 +54,7 @@ const InfoCard = () => {
   };
 
   return (
-    <div className="flex flex-col md:w-[40%] mx-auto h-screen overflow-y-auto p-7 bg-yellow-200">
+    <div className="flex flex-col md:w-[40%] mx-auto h-screen overflow-y-auto p-3 bg-yellow-200">
       <img
         src={logo}
         alt="Wordio Logo"
