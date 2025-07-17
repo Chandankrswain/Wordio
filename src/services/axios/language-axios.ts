b4938eac49534f3fdf11ecec1eb3806315764fc0
chandankrswain <mr.chandanswain@gmail.com>
2025-07-18 02:15:33 +0530

    ADD: translate and languages api

--- src/services/axios/language-axios.ts
+++ src/services/axios/language-axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://api.mymemory.translated.net/get",
+  baseURL: "https://libretranslate.com/",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
