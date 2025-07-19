4f4736bc4480d3b4c7fadf373302d2846408c85a
chandankrswain <mr.chandanswain@gmail.com>
2025-07-20 00:32:52 +0530

    some fixes

--- src/services/axios/language-axios.ts
+++ src/services/axios/language-axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://libretranslate.com/",
+  baseURL: "https://libretranslate.com/language",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
