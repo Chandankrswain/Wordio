4efc98184d144d7ca607715e60f0ee0ca3442801
chandankrswain <mr.chandanswain@gmail.com>
2025-07-21 01:01:26 +0530

    completed  a new component heading button

--- src/pages/text-to-text-translate.tsx
+++ src/pages/text-to-text-translate.tsx
@@ -1,11 +1,9 @@
 import TextBox from "../components/text-box";
 import logo from "../assets/logo.png";
-import { useState } from "react";
+import HeadingButton from "../components/heading-button";
 import { PiTextAaThin } from "react-icons/pi";
 
 const TextToTextTranslate = () => {
-  const [isClicked, setIsClicked] = useState(false);
-
   const TextBoxArray = [
     {
       placeholder: "Enter the text here................",
@@ -19,12 +17,6 @@ const TextToTextTranslate = () => {
     },
   ];
 
-  const handleClick = () => {
-    if (!isClicked) {
-      setIsClicked(true);
-    } else setIsClicked(false);
-  };
-
   return (
     <div className="flex flex-col md:w-[40%] mx-auto h-screen overflow-y-auto bg-yellow-200">
       <div className="h-26 flex justify-between mb-3">
@@ -34,25 +26,10 @@ const TextToTextTranslate = () => {
           className="w-44 h-10 ml-3 mt-8 mb-5"
           onClick={() => (window.location.href = "/")}
         />
-        <div className="flex justify-end items-center w-[50%] relative overflow-visible m-5">
-          <div className="flex flex-col items-center relative">
-            <button className="bg-[#F4F5F7] mr-5 border border-b-4 border-r-4 rounded rounded-2xl ">
-              <PiTextAaThin
-                onClick={handleClick}
-                className="w-7 h-7 m-2 cursor-pointer z-10"
-              />
-            </button>
-            <p
-              className={`absolute top-full mt-1 text-sm transition-all duration-500 ease-in-out mr-5 whitespace-nowrap ${
-                isClicked
-                  ? "translate-y-0 opacity-100"
-                  : "-translate-y-2 opacity-0"
-              }`}
-            >
-              Text Translate
-            </p>
-          </div>
-        </div>
+        <HeadingButton
+          icon={<PiTextAaThin className="w-7 h-7 m-2 cursor-pointer z-10" />}
+          label="Text Translate"
+        />
       </div>
       {TextBoxArray &&
         TextBoxArray.map((data, index) => (
