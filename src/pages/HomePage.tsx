ff7670c32fa2fd7e884de550b77c28e42c3256e3
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2025-07-23 19:07:06 +0530

    edited the home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import SearchBar from "../components/search-bar";
+import logo from "../assets/logo.png";
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col md:w-[40%] mx-auto justify-between h-screen p-2 bg-yellow-500">
+      <img src={logo} alt="Wordio Logo" className="w-44 h-10 ml-2 mt-6" />
+      <div className="flex flex-col">
+        <p className="text-[80px] font-bold leading-20">Search</p>
+        <p className="text-[40px] font-bold leading-20">Word</p>
+        <SearchBar />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
