87458517395482fd979f02294b776c6bf3d8ee88
chandankrswain <mr.chandanswain@gmail.com>
2025-08-02 01:47:43 +0530

    fix the feature button

--- src/components/feature-button.tsx
+++ src/components/feature-button.tsx
@@ -7,15 +7,14 @@ interface FeatureButtonProps {
 const FeatureButton = ({ icon, label, onClick }: FeatureButtonProps) => {
   return (
     <div
+      onClick={onClick}
       className="border border-r-5 border-b-5 rounded-[40px] w-[40vw] max-w-[100px] h-[100px] 
              flex flex-col items-center justify-center mt-2 
              active:translate-x-[2px] active:translate-y-[2px] 
              active:border-r-2 active:border-b-2 
              transition-transform duration-100 ease-in-out"
     >
-      <button className="mb-2" onClick={onClick}>
-        {icon}
-      </button>
+      <button className="mb-2">{icon}</button>
       <p className="text-sm">{label}</p>
     </div>
   );
